f0396ecbb7d855958b2ea87662ee8b84c3d8630e
ericcardenas52 <100006562+ericcardenas52@users.noreply.github.com>
2023-07-09 16:01:43 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+// JavaScript for the home page
+if (document.body.classList.contains('home-page')) {
+  // Add home page specific functionality here
+  console.log('This is the home page.');
+}
+
+// JavaScript for the details page
+if (document.body.classList.contains('details-page')) {
+  // Add details page specific functionality here
+  console.log('This is the details page.');
+}
+
+// JavaScript for the about page
+if (document.body.classList.contains('about-page')) {
+  // Add about page specific functionality here
+  console.log('This is the about page.');
+}
