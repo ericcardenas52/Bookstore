25b3c99d922177b9038e2225a36184bb9f0d161e
ericcardenas52 <100006562+ericcardenas52@users.noreply.github.com>
2023-07-09 15:43:51 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+// JavaScript code goes here
+window.addEventListener('DOMContentLoaded', function() {
+    // Simulated book data
+    const featuredBooksData = [
+      { title: 'Book 1', author: 'Author 1' },
+      { title: 'Book 2', author: 'Author 2' },
+      { title: 'Book 3', author: 'Author 3' },
+    ];
+  
+    const bookList = document.querySelector('.book-list');
+  
+    // Create featured books dynamically
+    featuredBooksData.forEach(book => {
+      const bookElement = document.createElement('div');
+      bookElement.classList.add('book');
+  
+      const titleElement = document.createElement('h3');
+      titleElement.textContent = book.title;
+  
+      const authorElement = document.createElement('p');
+      authorElement.textContent = `by ${book.author}`;
+  
+      bookElement.appendChild(titleElement);
+      bookElement.appendChild(authorElement);
+  
+      bookList.appendChild(bookElement);
+    });
+  });
+  
